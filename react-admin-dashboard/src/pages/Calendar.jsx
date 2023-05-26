4b89c20709b3f005dfefafa5fb09179cf26eaa13
Steven Duong <stevenduongx@gmail.com>
2023-05-26 15:22:54 -0600

    update kanban.jsx

--- react-admin-dashboard/src/pages/Calendar.jsx
+++ react-admin-dashboard/src/pages/Calendar.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month,
+import { ScheduleComponent, Day, Week, WorkWeek, Month,
 Agenda, Inject, Resize, DragAndDrop} from '@syncfusion/ej2-react-schedule';
 
-import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
 import { scheduleData } from '../data/dummy';
 import { Header } from '../components';
 
